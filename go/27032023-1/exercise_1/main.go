43e116efb72b1860b3205e9ac95f21b4dd38bc60
luisherreramald <luis.herreramaldonado@mercadolibre.com.mx>
2023-03-27 12:40:36 -0600

    add sentence exercise to each exercise

--- go/27032023-1/exercise_1/main.go
+++ go/27032023-1/exercise_1/main.go
@@ -1,3 +1,9 @@
+/*
+Una empresa de chocolates necesita calcular el impuesto de sus empleados al momento de depositar el sueldo, para cumplir el objetivo es necesario crear una 
+función que devuelva el impuesto de un salario. 
+Teniendo en cuenta que si la persona gana más de $50.000 se le descontará un 17 % del sueldo y si gana más de $150.000 se le descontará además un 10 % (27% en total).
+*/
+
 package main
 
 import (
--- go/27032023-1/exercise_2/main.go
+++ go/27032023-1/exercise_2/main.go
@@ -1,3 +1,8 @@
+/*
+Un colegio necesita calcular el promedio (por estudiante) de sus calificaciones. Se solicita generar una función en la cual se le pueda pasar N cantidad de enteros y 
+devuelva el promedio. No se pueden introducir notas negativas.
+*/
+
 package main
 
 import (
